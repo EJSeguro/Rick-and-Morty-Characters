bcfe1dd57ffb179dcaaae1dbd4397ec39f8c5e0d
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2025-11-12 17:33:26 -0300

    Correção Modal favoritos

--- rick-and-morty/src/views/FavoritesView.vue
+++ rick-and-morty/src/views/FavoritesView.vue
@@ -1,9 +1,23 @@
 <script setup>
 import { useFavoritesCharactersStore } from '@/stores/FavoritesCharacters.js'
-
+import { onMounted, ref } from 'vue'
 import CharacterCard from '@/components/Charactercard.vue'
+import CharacterModal from '@/components/CharacterModal.vue'
 
 const favoritesStore = useFavoritesCharactersStore()
+const showModal = ref(false)
+
+const selectedCharacter = ref(null)
+
+const openModal = (character) => {
+  selectedCharacter.value = character
+  showModal.value = true
+}
+
+onMounted(() => {
+  favoritesStore.favorites
+})
+
 </script>
 
 <template>
@@ -13,38 +27,39 @@ const favoritesStore = useFavoritesCharactersStore()
 
     <ul class="grid-container">
       <li v-for="character in favoritesStore.favorites" :key="character.id" class="character">
-        <CharacterCard :character="character" />
+        <CharacterCard :character="character" @click="openModal(character)" @select="selectedCharacter = $event" />
       </li>
     </ul>
+    <CharacterModal v-model="showModal" :character="selectedCharacter" />
   </section>
 </template>
 
 <style scoped>
 .characters-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
 }
 
 .characters-container h2 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
+  font-size: 2rem;
+  margin-bottom: 1rem;
 }
 
 .characters-container p {
-    font-size: 1.2rem;
-    color: #555;
+  font-size: 1.2rem;
+  color: #555;
 }
 
 .grid-container {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 3rem;
-    padding: 2rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 3rem;
+  padding: 2rem;
 }
 
 li {
-    list-style: none;
+  list-style: none;
 }
 </style>
